96e28e92caa6c2f89239ad71ee1d753387c25596
pashpitha <121786468+pashpitha@users.noreply.github.com>
2024-02-12 22:00:56 +0530

    import java.util.*; public class Solution{     public static void main(String[]args){         Scanner read=new Scanner(System.in);         int a=read.nextInt();         int b=read.nextInt();         int c;         c=(a*5)+(b*7);         System.out.printf("%d",c);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[]args){
+        Scanner read=new Scanner(System.in);
+        int a=read.nextInt();
+        int b=read.nextInt();
+        int c;
+        c=(a*5)+(b*7);
+        System.out.printf("%d",c);
+    }
+}
