136737cfa208d3eb4d2b857f01825240e9472739
pashpitha <121786468+pashpitha@users.noreply.github.com>
2024-02-13 09:52:28 +0530

    import java.util.*; public class volume{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         double vol;         vol=1.3333333*3.14*a*a*a;         System.out.printf("%.2f",vol);     } }

--- Volume_of_Sphere.java
+++ Volume_of_Sphere.java
@@ -1,10 +1,10 @@
 import java.util.*;
-public class Volume{
-    public static void main(String[]main){
+public class volume{
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int r=sc.nextInt();
+        int a=sc.nextInt();
         double vol;
-        vol=(1.333333*3.14*r*r*r);
+        vol=1.3333333*3.14*a*a*a;
         System.out.printf("%.2f",vol);
     }
 }
