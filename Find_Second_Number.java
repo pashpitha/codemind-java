4e7e9794abc9d838f4932be038c4a25234209313
pashpitha <121786468+pashpitha@users.noreply.github.com>
2024-02-16 11:29:17 +0530

    import java.util.*; public class number{     public static void main(String[]args){         Scanner sc=new Scanner(System.in);         int avg=sc.nextInt();         int x=sc.nextInt();         int y;         y=(2*avg)-x;         System.out.printf("%d",y);     } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class number{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int avg=sc.nextInt();
+        int x=sc.nextInt();
+        int y;
+        y=(2*avg)-x;
+        System.out.printf("%d",y);
+    }
+}
