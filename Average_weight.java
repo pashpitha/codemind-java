34423c7fb6fa9dccd1c31bf3c5f16707d01bd1cd
pashpitha <121786468+pashpitha@users.noreply.github.com>
2024-02-17 19:02:37 +0530

    import java.util.*; public class weight{     public static void main(String[]args){         Scanner sc=new Scanner(System.in);         int avg=sc.nextInt();         int a=sc.nextInt();         int b=sc.nextInt();         int c;         c=(3*avg)-(a+b);         System.out.printf("%d",c);     } }

---
+++ Average_weight.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class weight{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int avg=sc.nextInt();
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c;
+        c=(3*avg)-(a+b);
+        System.out.printf("%d",c);
+    }
+}
