00fe7bb62ba1482824a918205b03c464a44ea2f6
pashpitha <121786468+pashpitha@users.noreply.github.com>
2024-02-17 18:36:31 +0530

    import java.util.*; public class Solution{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         double a;         a = n*0.277778;         System.out.printf("%.2f",a);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        double a;
+        a = n*0.277778;
+        System.out.printf("%.2f",a);
+    }
+}
