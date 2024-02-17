825ba173b3ebdf5fc9296988319cb8687a036afa
pashpitha <121786468+pashpitha@users.noreply.github.com>
2024-02-17 18:55:27 +0530

    import java.util.*; public class Solutions{     public static void main(String[]args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=sc.nextInt();         int d;         d=a*b*c;         System.out.printf("%d KB",d);     } }

---
+++ Capacity.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solutions{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d;
+        d=a*b*c;
+        System.out.printf("%d KB",d);
+    }
+}
